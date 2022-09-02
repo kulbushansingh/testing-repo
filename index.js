143649fe358048836e7ae9c441e0478782cfeead
kulbushan singh <110520677+kulbushansingh@users.noreply.github.com>
2022-09-02 08:30:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if(randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩play 1 wins!";
 }
  else if(randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "play 1 wins!🚩";
+    document.querySelector("h1").innerHTML = "play 2 wins!🚩";
 }
 else {
     document.querySelector("h1").innerHTML = "draw!";
